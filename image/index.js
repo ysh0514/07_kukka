4a89eb41a58b30d98295681d530388caa0ec23d8
양성호 <sungho@yangseonghoui-MacBookAir.local>
2022-02-21 21:46:12 +0900

    Add : 레이아웃 및 mock 데이터 구현 완료

--- image/index.js
+++ image/index.js
@@ -1,1 +1,61 @@
 console.log("연결");
+
+const productList = [
+  {
+    productName: "클래식 핸드타이트",
+    date="(2/28~3/6)",
+    productPrice: "63,000",
+    productPlace:[{
+      place:"광화문점",
+      color:"#9d80d8",
+    },{
+      place:"구로점",
+      color:"#68a4fe"
+    },{
+      place:"송파점",
+      color:"#ffcd32"
+    },{    
+      place:"부산동래점",
+      color:"#00afe3" 
+    }]
+  },
+  {
+    productName: "오아시스 리스",
+    date="(2/21~2/27)",
+    productPrice: "79,000",
+    productPlace:[{
+      place:"광화문점",
+      color:"#9d80d8",
+    },{
+      place:"잠실점",
+      color:"#70cca9"
+    },{
+      place:"월계점",
+      color:"#ffa04d"
+    },{
+      place:"구로점",
+      color:"#68a4fe"
+    }]
+  },
+  {
+    productName: "플라워 클래스",
+    date="(2/18~2/22)",
+    productPrice: "84,000",
+    productPlace:[{
+      place:"광화문점",
+      color:"#9d80d8",
+    },{
+      place:"잠실점",
+      color:"#70cca9"
+    },{
+      place:"구로점",
+      color:"#68a4fe"
+    },{
+      place:"송파점",
+      color:"#ffcd32"
+    }]
+  },
+];
+
+const a = "Aaa"
+console.log(a)
