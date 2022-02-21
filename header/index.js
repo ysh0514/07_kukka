78e8465daaf40817356806cd4f506cef3b0d4b47
gilpop8663 <wolfye@naver.com>
2022-02-21 22:18:24 +0900

    Add - Header 추가, iframe 기능 절반 이상 구현

--- header/index.js
+++ header/index.js
@@ -1,1 +1,12 @@
-console.log("연결");
+const header = document.querySelector(".headerContainer");
+
+function onScroll() {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset > 200) {
+    header.classList.add("hasShadow");
+  } else {
+    header.classList.remove("hasShadow");
+  }
+}
+
+window.addEventListener("scroll", onScroll);
