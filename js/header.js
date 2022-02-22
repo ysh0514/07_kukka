6f5d4acd085b87b96aff43b544ab4df2cd779bed
gilpop8663 <wolfye@naver.com>
2022-02-22 17:39:53 +0900

    Add - 중간 취합 파일 추가

--- js/header.js
+++ js/header.js
@@ -1,11 +1,27 @@
 const header = document.querySelector(".headerContainer");
+const headerContainer = document.querySelector(".headerWidthContainer");
+const headerList = document.querySelector(".headerList");
+const headerVideo = document.querySelector(".headerVideo");
+const headerFlowerClass = document.querySelector(".headerFlowerClass");
 
 function onScroll() {
   if (window.pageYOffset > 200) {
-    header.classList.add("hasShadow");
+    headerContainer.classList.add("hasShadow");
+    headerList.classList.add("headerScrollList");
   } else {
-    header.classList.remove("hasShadow");
+    headerContainer.classList.remove("hasShadow");
+    headerList.classList.remove("headerScrollList");
   }
 }
 
+function onVideoScroll() {
+  window.scrollTo(0, 600);
+}
+
+function onFlowerClassScroll() {
+  window.scrollTo(0, 800);
+}
+
 window.addEventListener("scroll", onScroll);
+headerVideo.addEventListener("click", onVideoScroll);
+headerFlowerClass.addEventListener("click", onFlowerClassScroll);
