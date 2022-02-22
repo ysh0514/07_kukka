62337211df8520d665e521fd91655b242cf032af
gilpop8663 <wolfye@naver.com>
2022-02-22 16:28:12 +0900

    Add - 취합 중인 파일 추가

--- js/header.js
+++ js/header.js
@@ -1,7 +1,6 @@
 const header = document.querySelector(".headerContainer");
 
 function onScroll() {
-  console.log(window.pageYOffset);
   if (window.pageYOffset > 200) {
     header.classList.add("hasShadow");
   } else {
